9c32326c8e748baa06817c5ccf8892279c4769f5
Giuseppe <peppelg1234@gmail.com>
2018-04-13 19:22:57 +0200

    example

--- example.php
+++ example.php
@@ -1,6 +1,9 @@
 <?php
 require_once 'EzTG.php';
 $callback = function($update, $EzTG) {
-  $EzTG->sendMessage(array('chat_id' => $update->message->from->id, 'text' => 'wewe'));
+  if (isset($update->message->text) and $update->message->text == '/start') {
+    $EzTG->sendMessage(array('chat_id' => $update->message->from->id, 'text' => 'wewe'));
+  }
+  
 };
 $EzTG = new EzTG(array('token' => 'token', 'callback' => $callback));
